75048a0b8593f5213246950ab2587168594194fa
Ahmed Ghazal <a.h.ghazal90@gmail.com>
2021-02-15 13:39:20 +0100

    Create a new users app to manage users authentication
    Create a login page and add a link to it in the base HTML.
    Also add a logout link that is only displayed when a user
    is already logged in.

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+"""Define URL patterns for users"""
+
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    # Login page
+    url('login', LoginView.as_view(template_name='users/login.html'), name='login'),
+    url('logout', views.logout_view, name='logout'),
+]
